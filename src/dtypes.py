e0f01d52114da7ef8dc23f80a95e40aee8167aec
chrimp <60501603+chrimp@users.noreply.github.com>
2025-06-09 02:33:28 +0900

    Create dtypes.py

---
+++ src/dtypes.py
@@ -0,0 +1,12 @@
+from construct import Struct, Int8ul, Int8sl, Int16ul, Int16sl, Int32ul, Float32l, Float64l, Int64ul, Int8sl
+
+uint8 = Int8ul
+uint16 = Int16ul
+uint32 = Int32ul
+uint64 = Int64ul
+
+int8 = Int8sl
+int16 = Int16sl
+
+float32 = Float32l
+double = Float64l
